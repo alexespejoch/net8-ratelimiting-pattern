75a8735ba031ab69585d1f0cf744ae832891d61e
Espejo Chavarría, Alex <alex.espejo.c@gmail.com>
2024-09-06 18:17:26 -0500

    Initial commit

---
+++ src/BookStore.Domain/Interfaces/IBookService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using BookStore.Domain.Models;
+
+namespace BookStore.Domain.Interfaces
+{
+    public interface IBookService
+    {
+        Task<IEnumerable<Book>> GetAll(CancellationToken cancellationToken = default);
+        Task<Book> GetById(int id, CancellationToken cancellationToken = default);
+        Task<Book> Add(Book book, CancellationToken cancellationToken = default);
+        Task<Book> Update(Book book, CancellationToken cancellationToken = default);
+        Task<bool> Remove(Book book, CancellationToken cancellationToken = default);
+        Task<IEnumerable<Book>> GetBooksByCategory(int categoryId, CancellationToken cancellationToken = default);
+        Task<IEnumerable<Book>> Search(string bookName, CancellationToken cancellationToken = default);
+        Task<IEnumerable<Book>> SearchBookWithCategory(string searchedValue, CancellationToken cancellationToken = default);
+    }
+}
---
+++ src/BookStore.WebApi/Program.cs
@@ -0,0 +1,50 @@
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.RateLimiting;
+using System.Threading.RateLimiting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(opts =>
+{
+    opts.SwaggerDoc("v1", new OpenApiInfo()
+    {
+        Title = "BookStore API",
+        Version = "v1"
+    });
+});
+
+builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.RegisterInfrastureDependencies(builder.Configuration);
+
+var fixedWindowPolicy = "fixedWindow";
+builder.Services.AddRateLimiter(configureOptions => {
+    configureOptions.AddFixedWindowLimiter(policyName: fixedWindowPolicy, options =>
+    {
+        options.PermitLimit = 4;
+        options.Window = TimeSpan.FromSeconds(30);
+        options.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+        options.QueueLimit = 0;
+    });
+    configureOptions.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
+});
+
+var concurrencyPolicy = "Concurrency";
+builder.Services.AddRateLimiter(configureOptions => {
+    configureOptions.AddConcurrencyLimiter(policyName: concurrencyPolicy, options =>
+    {
+        options.PermitLimit = 2;
+        options.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+        options.QueueLimit = 0;
+    });
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseAuthorization();
+app.UseRateLimiter();
+app.MapControllers();
+app.Run();
